36d306bb9e6eb7f34049a490c30c8a1e5e8a7cd4
Naimur Rahman <naimur2231@gmail.com>
2023-06-05 21:27:19 +0600

    menu crud operation and secure api

--- index.js
+++ index.js
@@ -139,6 +139,21 @@ async function run() {
         res.send(result);
     })
 
+    // post api menu with specific admin role
+    app.post('/menu', verifyJWT, verifyAdmin, async(req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    })
+
+    // delete api with menu 
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // get all reviews data 
     app.get('/reviews', async(req, res) => {
         const result = await reviewsCollection.find().toArray();
